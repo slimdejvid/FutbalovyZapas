ce2e3a89f4fe689b5e24d911febb847c8417d8c1
slimdejvid <david.branny14@gmail.com>
2019-12-10 19:10:28 +0100

    final refaktoring na priebezne odovzdanie

--- projekt_futbalovy_zapas/src/sk/stuba/branny/futbal/match/Match.java
+++ projekt_futbalovy_zapas/src/sk/stuba/branny/futbal/match/Match.java
@@ -173,6 +173,7 @@ public class Match {
 		return null;
 	}
 
+	
 	public int returnPosition(Player p) {
 		int numberOfPlayers = startingElevenMyTeam.size();
 
@@ -184,6 +185,7 @@ public class Match {
 		return 0;
 	}
 
+	
 	public void exchangePlayers(List<Player> players, Player player1, Player player2) {
 		int pozicia =returnPosition(player1);
 		startingElevenMyTeam.remove(pozicia);
@@ -192,6 +194,7 @@ public class Match {
 		players.add(player1);
 		
 	}
+	
 	
 	public void substitute(Goalkeeper g1, Goalkeeper g2) {
 		Player goalie = (Player) g1;
@@ -206,35 +209,28 @@ public class Match {
 
 	}
 
-	public void substitute(Defender d1, Defender d2) {
-		int pozicia = returnPosition(d1);
-
+	
+	public void substitute(Defender defender1, Defender defender2) {
 		List<Player> players = homeTeam.getAvailableDefenders();
 
-		startingElevenMyTeam.remove(pozicia);
-		startingElevenMyTeam.add(pozicia, d2);
-		players.remove(d2);
-		players.add(d1);
+		exchangePlayers(players, defender1, defender2);
 
-		System.out.println("SUBSTITUTION" + "\nON: " + d2.getFirstName() + " " + d2.getSecondName() + "\nOFF: "
-				+ d1.getFirstName() + " " + d1.getSecondName());
+		System.out.println("SUBSTITUTION" + "\nON: " + defender2.getFirstName() + " " + defender2.getSecondName() + "\nOFF: "
+				+ defender1.getFirstName() + " " + defender1.getSecondName());
 	}
 
+	
 	public void substitute(Midfielder midfielder1, Midfielder midfielder2) {
-		
 		List<Player> players = homeTeam.getAvailableMidfielders();
 		
 		exchangePlayers(players, midfielder1, midfielder2);
 
 		System.out.println("SUBSTITUTION" + "\nON: " + midfielder2.getFirstName() + " " + midfielder2.getSecondName() + "\nOFF: "
 				+ midfielder1.getFirstName() + " " + midfielder1.getSecondName());
-
 	}
 
+	
 	public void substitute(Forward forward1, Forward forward2) {
-
-		
-
 		List<Player> players = homeTeam.getAvailableForwards();
 
 		exchangePlayers(players, forward1, forward2);
@@ -243,6 +239,97 @@ public class Match {
 				+ forward1.getFirstName() + " " + forward1.getSecondName());
 	}
 
+	
+	public boolean forwardVsDefender(Player forward, Player defender) {
+		int defValue, dribValue;
+
+		defValue = (int) ((Defender) defender).getDefending();
+
+		dribValue = (int) ((Forward) forward).getDribling();
+
+		if (dribValue > defValue) {
+			System.out.println("- " + forward.getFirstName() + " " + forward.getSecondName() + " has dribbled past "
+					+ defender.getFirstName() + " " + defender.getSecondName());
+			return true;
+		} 
+		else {
+			forward.setHasBall(false);
+			defender.setHasBall(true);
+			System.out.println("- " + defender.getFirstName() + " " + defender.getSecondName()
+					+ " has taken the ball from " + forward.getFirstName() + " " + forward.getSecondName());
+
+			return false;
+		}
+	}
+	
+	public void shot(Player shooter, Player goalkeeper) {
+		int shootingValue, goalkeepingValue;
+		boolean homePlayer;
+
+		shootingValue = (int) ((Forward) shooter).getShooting();
+		goalkeepingValue = (int) ((Goalkeeper) goalkeeper).getGoalkeeping();
+
+		if (startingElevenMyTeam.contains(shooter)) { // If shooter is in my team
+			homePlayer = true;
+		} 
+		else {
+			homePlayer = false;
+		}
+
+		Random rnd = new Random();
+		int random = rnd.nextInt(60) + 40;
+		int random2 = rnd.nextInt(60) + 40;
+
+		shooter.setHasBall(false);
+		if (homePlayer) {
+			if (random <= shootingValue){		// If my forward hits the post
+			
+				if (random2 > goalkeepingValue) {
+					skoreHome++;
+
+					startingElevenAwayTeam.get(5).setHasBall(true);
+					System.out.println(
+							"- " + shooter.getFirstName() + " " + shooter.getSecondName() + " shoots and SCORES!!!!");
+					System.out.println("- Score is " + skoreHome + " : " + skoreAway);
+				} 
+				else {
+					System.out.println("- " + shooter.getFirstName() + " " + shooter.getSecondName() + " shoots but "
+							+ goalkeeper.getFirstName() + " " + goalkeeper.getSecondName() + " has made great save!!!");
+					goalkeeper.setHasBall(true);
+				}
+			} 
+			else {
+				goalkeeper.setHasBall(true);
+				System.out.println(
+						"- " + shooter.getFirstName() + " " + shooter.getSecondName() + " shoots but misses the goal");
+			}
+		} 
+		else if (!homePlayer) {
+			if (random <= shootingValue){		// If enemy forward hits the post
+			
+				if (random2 > goalkeepingValue) {
+					skoreAway++;
+
+					startingElevenMyTeam.get(5).setHasBall(true);
+					System.out.println(
+							"- " + shooter.getFirstName() + " " + shooter.getSecondName() + " shoots and SCORES!!!!");
+					System.out.println("- Score is " + skoreHome + " : " + skoreAway);
+				}
+				else {
+					System.out.println("- " + shooter.getFirstName() + " " + shooter.getSecondName() + " shoots but "
+							+ goalkeeper.getFirstName() + " " + goalkeeper.getSecondName() + " has made great save!!!");
+					goalkeeper.setHasBall(true);
+
+				}
+			} 
+			else {
+				goalkeeper.setHasBall(true);
+				System.out.println(
+						"- " + shooter.getFirstName() + " " + shooter.getSecondName() + " shoots but misses the goal");
+			}
+		}
+	}
+	
 	public void pass(Player passing, Player recieving, Player defending) {
 		int passValue;
 		Random rnd = new Random();
@@ -262,116 +349,33 @@ public class Match {
 			System.out.println("- Accurate pass to " + recieving.getFirstName() + " " + recieving.getSecondName());
 		}
 	}
-
 	
-	
-	
-	public int forwardVsDefender(Player forward,Player defender) 
-	{
-		int defValue,dribValue;
+	public void passingSituation(int playerPosition, List<Player> team1, List<Player> team2) {
 		
-		defValue = (int)((Defender)defender).getDefending();
-		
-		dribValue = (int)((Forward)forward).getDribling();
-
-		if(dribValue > defValue)
-		{
-			System.out.println("- "+forward.getFirstName()+" "+forward.getSecondName()+" has dribbled past "+defender.getFirstName()+" "+defender.getSecondName());
-			return 1;
-		}	
-		else 
-		{
-			forward.setHasBall(false);
-			defender.setHasBall(true);
-			System.out.println("- "+defender.getFirstName()+" "+defender.getSecondName()+" has taken the ball from "+forward.getFirstName()+" "+forward.getSecondName());
-
-			return 0;
-		}
-			
-	}
-	
-	public void shot(Player shooter, Player goalkeeper) //strielanie na bránu
-	{
-		int shootingValue,goalkeepingValue;
-		boolean homePlayer;
-		
-		shootingValue = (int)((Forward)shooter).getShooting();
-		goalkeepingValue = (int)((Goalkeeper)goalkeeper).getGoalkeeping();
-		
-		if(startingElevenMyTeam.contains(shooter)) //ak strila domaci utoènik
-			homePlayer=true;
-		else
-			homePlayer=false;
-			
+		System.out.println(team1.get(playerPosition).getFirstName() + " "
+				+ team1.get(playerPosition).getSecondName() + " has the ball");
 		
 		Random rnd = new Random();
-		int random = rnd.nextInt(60)+40;
-		int random2 = rnd.nextInt(60)+40;
+		int numberOfPlayers=team1.size();
+		int random = (rnd.nextInt(numberOfPlayers - 1 - playerPosition) + (playerPosition + 1));
 		
-		shooter.setHasBall(false);
-			if(random < shootingValue && homePlayer)//ak trafil branku domaci
-			{
-				
-				if(random2 > goalkeepingValue) // ak brankar nechytil strelu domaceho
-				{
-					skoreHome++;
-					
-					startingElevenAwayTeam.get(5).setHasBall(true);
-					System.out.println("- "+shooter.getFirstName()+" "+shooter.getSecondName()+" shoots and SCORES!!!!");
-					System.out.println("- Score is "+skoreHome+" : "+skoreAway);
-				}
-				else //ak chyti
-				{
-					System.out.println("- "+shooter.getFirstName()+" "+shooter.getSecondName()+" shoots but "+goalkeeper.getFirstName()+" "+goalkeeper.getSecondName()+" has made great save!!!");
-					goalkeeper.setHasBall(true);
-				}		
-			}
-			else if(random > shootingValue && homePlayer)// ak netrafil branku domaci
-			{
-				goalkeeper.setHasBall(true);
-				System.out.println("- "+shooter.getFirstName()+" "+shooter.getSecondName()+" shoots but misses the goal");
-			}
-			else if(random < shootingValue && !homePlayer)// ak trafi branku hostujuci hrac
-			{
-				if(random2 > goalkeepingValue) // ak brankar nechytil strelu hostujuceho 
-				{
-					skoreAway++;
-					
-					startingElevenMyTeam.get(5).setHasBall(true);
-					System.out.println("- "+shooter.getFirstName()+" "+shooter.getSecondName()+" shoots and SCORES!!!!");
-					System.out.println("- Score is "+skoreHome+" : "+skoreAway);
-				}
-				else //ak chyti
-				{
-					System.out.println("- "+shooter.getFirstName()+" "+shooter.getSecondName()+" shoots but "+goalkeeper.getFirstName()+" "+goalkeeper.getSecondName()+" has made great save!!!");
-					goalkeeper.setHasBall(true);
-					
-				}		
-			}
-			else //ak hostujuci netrafi branku
-			{
-				goalkeeper.setHasBall(true);
-				System.out.println("- "+shooter.getFirstName()+" "+shooter.getSecondName()+" shoots but misses the goal");
-			}
-
-		
+		pass(team1.get(playerPosition), team1.get(random), team2.get(numberOfPlayers - playerPosition - 1)); 
 	}
 	
-	public void startTheMatch() // metoda ziskavajuca priebeh zapasu
-	{
+	public void startTheMatch() {
+
 		int numberOfSubs;
-		int numberOfActions = 20; // poèet akcii zapasu
-		Random rnd = new Random();
+		int numberOfActions = 20;
 
 		int numberOfPlayers = startingElevenMyTeam.size();
+		boolean dribbled;
 
-		int drible;
-
-		startingElevenMyTeam.get(5).setHasBall(true);// na zaèiatok ma loptu hraè z mojho teamu
+		startingElevenMyTeam.get(5).setHasBall(true);
 
 		System.out.println("The match has started");
 
 		while (numberOfActions > 0) {
+
 			if (numberOfActions == 15) {
 				System.out.println("Choose how many substitutions you want to make");
 
@@ -380,122 +384,63 @@ public class Match {
 			}
 
 			System.out.println("\n");
-			for (int i = 0; i < numberOfPlayers; i++) {
+			for (int position = 0; position < numberOfPlayers; position++) {
+				if (startingElevenMyTeam.get(position).getHasBall() == true) {
+					
+					if (startingElevenMyTeam.get(position) instanceof Forward) {
 
-				if (startingElevenMyTeam.get(i).getHasBall() == true
-						&& startingElevenMyTeam.get(i) instanceof Goalkeeper) // ak má moj brankár loptu
-				{
-					System.out.println(startingElevenMyTeam.get(i).getFirstName() + " "
-							+ startingElevenMyTeam.get(i).getSecondName() + " has the ball");
+						System.out.println(startingElevenMyTeam.get(position).getFirstName() + " "
+								+ startingElevenMyTeam.get(position).getSecondName() + " has the ball");
 
-					int random = (rnd.nextInt(numberOfPlayers - 1 - i) + (i + 1));
+						dribbled = forwardVsDefender(startingElevenMyTeam.get(position),
+								startingElevenAwayTeam.get(numberOfPlayers - position));
 
-					pass(startingElevenMyTeam.get(i), startingElevenMyTeam.get(random),
-							startingElevenAwayTeam.get(numberOfPlayers - i - 1)); // tak prihráva
-																									// náhodnému hráèovi
-																									// pred ním
+						if (dribbled) {
+							shot(startingElevenMyTeam.get(position), startingElevenAwayTeam.get(0));
+						}
 
-				} else if (startingElevenMyTeam.get(i).getHasBall() == true
-						&& startingElevenMyTeam.get(i) instanceof Defender) // ak má moj obránca loptu
-				{
-
-					System.out.println(startingElevenMyTeam.get(i).getFirstName() + " "
-							+ startingElevenMyTeam.get(i).getSecondName() + " has the ball");
-
-					int random = (rnd.nextInt(numberOfPlayers - 1 - i) + (i + 1));
-
-					pass(startingElevenMyTeam.get(i), startingElevenMyTeam.get(random),
-							startingElevenAwayTeam.get(numberOfPlayers - i - 1)); // prihráva náhodnému
-																									// hráèovi pred ním
-
-				} else if (startingElevenMyTeam.get(i).getHasBall() == true
-						&& startingElevenMyTeam.get(i) instanceof Midfielder)// podobne aj pre stredných záložníkov
-				{
-
-					System.out.println(startingElevenMyTeam.get(i).getFirstName() + " "
-							+ startingElevenMyTeam.get(i).getSecondName() + " has the ball");
-
-					int random = (rnd.nextInt(numberOfPlayers - 1 - i) + (i + 1));
-
-					pass(startingElevenMyTeam.get(i), startingElevenMyTeam.get(random),
-							startingElevenAwayTeam.get(numberOfPlayers - i - 1));
-
-				} else if (startingElevenMyTeam.get(i).getHasBall() == true
-						&& startingElevenMyTeam.get(i) instanceof Forward) {
-
-					System.out.println(startingElevenMyTeam.get(i).getFirstName() + " "
-							+ startingElevenMyTeam.get(i).getSecondName() + " has the ball");
-
-					drible = forwardVsDefender(startingElevenMyTeam.get(i),
-							startingElevenAwayTeam.get(numberOfPlayers - i)); // prechádza cez hráè ktorý je mu
-																				// pridelený(prideleny je vzdy obranca)
-
-					if (drible == 1)// ak prešiel
-					{
-						shot(startingElevenMyTeam.get(i), startingElevenAwayTeam.get(0));// vystrelí na branku
 					}
+					else{
 
-				} else if (startingElevenAwayTeam.get(i).getHasBall() == true
-						&& startingElevenAwayTeam.get(i) instanceof Goalkeeper) // ak nepriatelsky brankar ma loptu
-				{
+						passingSituation(position, startingElevenMyTeam, startingElevenAwayTeam);
+					} 
 
-					System.out.println(startingElevenAwayTeam.get(i).getFirstName() + " "
-							+ startingElevenAwayTeam.get(i).getSecondName() + " has the ball");
+				} 
+				else if (startingElevenAwayTeam.get(position).getHasBall() == true) {
 
-					int random = (rnd.nextInt(numberOfPlayers - 1 - i) + (i + 1));
+					if (startingElevenAwayTeam.get(position) instanceof Forward) {
 
-					pass(startingElevenAwayTeam.get(i), startingElevenAwayTeam.get(random),
-							startingElevenMyTeam.get(numberOfPlayers - i - 1));
+						System.out.println(startingElevenAwayTeam.get(position).getFirstName() + " "
+								+ startingElevenAwayTeam.get(position).getSecondName() + " has the ball");
 
-				} else if (startingElevenAwayTeam.get(i).getHasBall() == true
-						&& startingElevenAwayTeam.get(i) instanceof Defender) {
+						dribbled = forwardVsDefender(startingElevenAwayTeam.get(position),
+								startingElevenMyTeam.get(numberOfPlayers - position));
 
-					System.out.println(startingElevenAwayTeam.get(i).getFirstName() + " "
-							+ startingElevenAwayTeam.get(i).getSecondName() + " has the ball");
+						if (dribbled) {
+							shot(startingElevenAwayTeam.get(position), startingElevenMyTeam.get(0));
+						}
 
-					int random = (rnd.nextInt(numberOfPlayers - 1 - i) + (i + 1));
-
-					pass(startingElevenAwayTeam.get(i), startingElevenAwayTeam.get(random),
-							startingElevenMyTeam.get(numberOfPlayers - i - 1));
-
-				} else if (startingElevenAwayTeam.get(i).getHasBall() == true
-						&& startingElevenAwayTeam.get(i) instanceof Midfielder) {
-					System.out.println(startingElevenAwayTeam.get(i).getFirstName() + " "
-							+ startingElevenAwayTeam.get(i).getSecondName() + " has the ball");
-
-					int random = (rnd.nextInt(numberOfPlayers - 1 - i) + (i + 1));
-
-					pass(startingElevenAwayTeam.get(i), startingElevenAwayTeam.get(random),
-							startingElevenMyTeam.get(numberOfPlayers - i - 1));
-
-				} else if (startingElevenAwayTeam.get(i).getHasBall() == true
-						&& startingElevenAwayTeam.get(i) instanceof Forward) {
-
-					System.out.println(startingElevenAwayTeam.get(i).getFirstName() + " "
-							+ startingElevenAwayTeam.get(i).getSecondName() + " has the ball");
-
-					drible = forwardVsDefender(startingElevenAwayTeam.get(i),
-							startingElevenMyTeam.get(numberOfPlayers - i));
-
-					if (drible == 1) {
-						shot(startingElevenAwayTeam.get(i), startingElevenMyTeam.get(0));
 					}
-
+					else{
+					
+						passingSituation(position, startingElevenAwayTeam, startingElevenMyTeam);
+					}
 				}
+
 			}
 			--numberOfActions;
 		}
+
 		if (skoreHome < skoreAway) {
 			System.out.println("\nFinal whistle!. The result is " + skoreHome + " : " + skoreAway + " The winner is "
 					+ awayName + awayTeam.toString());
-
-		}
-
-		else if (skoreHome == skoreAway)
+		} else if (skoreHome == skoreAway) {
 			System.out.println("\nFinal whistle!. The result is " + skoreHome + " : " + skoreAway);
-		else
+		} else {
 			System.out.println(
 					"\nFinal whistle!. The result is " + skoreHome + " : " + skoreAway + " The winner is " + homeName);
+		}
+
 	}
 	
 }
